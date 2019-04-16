5a75a6861ad4e3cbc30d4feda5408da698aff076
--replace-all <bethel.hall33@gmail.com>
2019-04-16 16:54:31 +0300

    Complex number adding function added

---
+++ complex-number-adder.py
@@ -0,0 +1,31 @@
+import cmath
+
+
+def add_complex_numbers(real_num1, imaginary_num1, real_num2, imaginary_num2):
+    real_num1 = eval(input("Enter the first real number: "))
+    imaginary_num1 = eval(input("Enter the first imaginary number"))
+    real_num2 = eval(input("Enter the second real number: "))
+    imaginary_num2 = eval(input("Enter the second imaginary number: "))
+    complex_1 = complex(real_num1, imaginary_num1)
+    complex_2 = complex(real_num2, imaginary_num2)
+
+    real1 = complex_1.real
+    real2 = complex_2.real
+
+    imaginary1 = complex_1.imag
+    imaginary2 = complex_2.imag
+
+    real_sum = real1 + real2
+    imag_sum = imaginary1 + imaginary2
+
+    result = complex(real_sum, imag_sum)
+    return result
+
+
+def main():
+    print("teting")
+    print("The result is: ", add_complex_numbers(2, 2, 3, 3))
+
+
+if __name__ == '__main__':
+    main()
