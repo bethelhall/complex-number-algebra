80130964e49e41bfc06b8c9e98b9981fd808aca0
Haylemicheal <hmicheal50@gmail.com>
2019-04-18 09:44:28 +0300

    Subtraction function is done

--- complex-number-adder.py
+++ complex-number-adder.py
@@ -6,6 +6,10 @@ def add_complex_numbers(real_num1, imaginary_num1, real_num2, imaginary_num2):
     complex_2 = complex(real_num2, imaginary_num2)
     sum = complex_1 +complex_2
     return sum
-
+def subtraction(a1,b1,a2,b2):
+    c1 = complex(a1,b1)
+    c2 = complex(a2, b2)
+    difference = c1-c2
+    return  difference
 
 
